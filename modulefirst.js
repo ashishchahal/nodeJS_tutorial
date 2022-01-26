21e491e09dcfd17145c92b43c7b33488aeb13eb3
Ashish Chahal <ashish.chahal27@gmail.com>
2022-01-26 08:18:02 +0530

    added & used ES6 import module and updated 'type':'module' in package.json

---
+++ modulefirst.js
@@ -0,0 +1,7 @@
+// const simple = require("./modulesecond");
+
+// import simple2, { simple } from "./modulesecond.mjs";
+import * as a2 from "./modulesecond.mjs";
+
+a2.simple();
+a2.simple2();
---
+++ modulesecond.mjs
@@ -0,0 +1,8 @@
+export function simple() {
+  console.log("simple is complex");
+}
+
+export function simple2() {
+  console.log("simple2 it is not simple");
+}
+// module.exports = simple;
