659507f10be9b80eac8206a45e1566b375f9546b
Ashish Chahal <ashish.chahal27@gmail.com>
2022-01-26 08:32:46 +0530

    url module functionality learned

---
+++ urlmodule.js
@@ -0,0 +1,9 @@
+import url from "url";
+
+const myUrl = new URL("https://example.org:8080");
+myUrl.pathname = "/a/b/c";
+myUrl.search = "?d=e";
+myUrl.hash = "#fgh";
+
+console.log(myUrl);
+console.log(myUrl.href);
